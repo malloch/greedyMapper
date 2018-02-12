1391dbd42f21dab1049694c4aceaa8ff40091c34
Joseph Malloch <joseph.malloch@gmail.com>
2018-02-12 17:54:02 -0400

    Updating to libmapper v1 api

--- main.c
+++ main.c
@@ -19,142 +19,167 @@
 #include "mapper/mapper.h"
 
 int done = 0;
-int port = 9000;
 
-mapper_device device;
-mapper_monitor monitor;
-mapper_db db;
+mapper_device dev;
+mapper_database db;
+int sig_counter = 0;
 
-void signalHandler(mapper_signal sig, mapper_db_signal props,
-                   mapper_timetag_t *timetag, void *value)
-{
-    mapper_signal sig_out = props->user_data;
-    msig_update(sig_out, value);
-}
-
-void linkHandler(mapper_db_link lnk, mapper_db_action_t a, void *user)
-{
-    if (a != MDB_NEW) return;
-    int length = strlen(mdev_name(device));
-    if (strncmp(lnk->src_name, mdev_name(device), length) == 0 ||
-        strncmp(lnk->dest_name, mdev_name(device), length) == 0)
-        return;
-    mapper_monitor_link(monitor, lnk->src_name, mdev_name(device));
-    mapper_monitor_link(monitor, mdev_name(device), lnk->dest_name);
-}
-
-void connectionHandler(mapper_db_connection con, mapper_db_action_t a, void *user)
+void mapHandler(mapper_database foo, mapper_map map, mapper_record_event e, const void *user)
 {
     /* Check if this applies to me. We will check if the name string matches without its ordinal
      * in case there are multiple copies of greedyMapper running. Without this safeguard two or
-     * more copies would re-route each other's mappings in an infinite loop. */
-    int length = strlen("/greedyMapper.");
-    if (strncmp(con->src_name, mdev_name(device), length) == 0 ||
-        strncmp(con->dest_name, mdev_name(device), length) == 0) {
-        // this connection involves me - don't interfere
-        printf("skipping connection %s -> %s\n", con->src_name, con->dest_name);
+     * more copies would re-route each other's maps in an infinite loop. */
+    int i, length = strlen("greedyMapper.");
+    mapper_slot slot;
+    mapper_signal sig;
+    mapper_device remote_dev;
+
+    if (e != MAPPER_ADDED)
         return;
+
+    for (i = 0; i < mapper_map_num_slots(map, MAPPER_LOC_ANY); i++) {
+        slot = mapper_map_slot(map, MAPPER_LOC_ANY, i);
+        sig = mapper_slot_signal(slot);
+        remote_dev = mapper_signal_device(sig);
+        if (remote_dev == dev) {
+            // this map involves me - don't interfere
+            printf("skipping map ");
+            mapper_map_print(map);
+            printf("\n");
+            return;
+        }
+        if (strncmp(mapper_device_name(remote_dev), "greedyMapper.", length)==0) {
+            // this map involves another copy of greedyMapper - don't interfere
+            printf("skipping map ");
+            mapper_map_print(map);
+            printf("\n");
+            return;
+        }
     }
-    switch (a) {
-        case MDB_NEW:
-        {
-            printf("got new connection! %s -> %s\n", con->src_name, con->dest_name);
-            char src[256], dest[256];
-            mapper_signal insig, outsig;
-            // check if we already have the output
-            outsig = mdev_get_output_by_name(device, con->dest_name, 0);
-            if (!outsig) {
-                // if not, create it
-                outsig = mdev_add_output(device, con->dest_name, con->dest_length, con->dest_type, 0, 0, 0);
-                mdev_add_input(device, con->dest_name, con->dest_length, con->dest_type, 0,
-                               (con->range.known | CONNECTION_RANGE_DEST_MIN) ? &con->range.dest_min : 0,
-                               (con->range.known | CONNECTION_RANGE_DEST_MAX) ? &con->range.dest_max : 0,
-                               signalHandler, outsig);
-            }
-            snprintf(dest, 256, "%s%s", mdev_name(device), con->dest_name);
-            // check if we already have the input
-            insig = mdev_get_input_by_name(device, con->src_name, 0);
-            if (!insig) {
-                // if not, create it
-                outsig = mdev_add_output(device, con->src_name, con->src_length, con->src_type, 0,
-                                         (con->range.known | CONNECTION_RANGE_SRC_MIN) ? &con->range.src_min : 0,
-                                         (con->range.known | CONNECTION_RANGE_SRC_MAX) ? &con->range.src_max : 0);
-                mdev_add_input(device, con->src_name, con->src_length, con->src_type,
-                               0, 0, 0, signalHandler, outsig);
-            }
-            snprintf(src, 256, "%s%s", mdev_name(device), con->src_name);
-            // create new connections
-            mapper_monitor_connect(monitor, con->src_name, src, 0, 0);
-            unsigned int flags = ((con->clip_min ? CONNECTION_CLIPMIN : 0) |
-                                  (con->clip_max ? CONNECTION_CLIPMAX : 0) |
-                                  con->range.known |
-                                  (con->expression ? CONNECTION_EXPRESSION : 0) |
-                                  (con->mode ? CONNECTION_MODE : 0) |
-                                  (con->muted ? CONNECTION_MUTED : 0));
-            mapper_monitor_connect(monitor, src, dest, con, flags);
-            mapper_monitor_connect(monitor, dest, con->dest_name, 0, 0);
-            mapper_monitor_disconnect(monitor, con->src_name, con->dest_name);
+
+    printf("got new map! ");
+    mapper_map_print(map);
+    printf("\n");
+
+    mapper_signal src, dst, local = 0;
+    src = mapper_slot_signal(mapper_map_slot(map, MAPPER_LOC_SOURCE, 0));
+    dst = mapper_slot_signal(mapper_map_slot(map, MAPPER_LOC_DESTINATION, 0));
+
+    // check if we have already mirrored source signal
+    mapper_map *maps = mapper_device_maps(dev, MAPPER_DIR_INCOMING);
+    while (maps && *maps) {
+        if (mapper_map_slot_by_signal(*maps, src)) {
+            local = mapper_map_slot(*maps, MAPPER_LOC_DESTINATION, 0);
             break;
         }
-        default:
-            break;
+        maps = mapper_map_query_next(maps);
     }
+
+    if (!local) {
+        char signame[16];
+        snprintf(signame, 16, "signal/%d", sig_counter++);
+        local = mapper_device_add_input_signal(dev, signame,
+                                               mapper_signal_length(src),
+                                               mapper_signal_type(src),
+                                               0, 0, 0, 0, 0);
+    }
+
+    // create "bypass" map from src to local signal
+    mapper_map tmp = mapper_map_new(1, &src, 1, &local);
+    mapper_map_push(tmp);
+
+    // create map from local to dst and copy all properties
+    tmp = mapper_map_new(1, &local, 1, &dst);
+    int num_props = mapper_map_num_properties(map);
+    char type;
+    const char *name;
+    const void *value;
+    for (i = 0; i < num_props; i++) {
+        mapper_map_property_index(map, i, &name, &length, &type, &value);
+        mapper_map_set_property(tmp, name, length, type, value, 1);
+    }
+    mapper_map_push(tmp);
+
+    // remove original map
+    mapper_map_release(map);
 }
 
 void createDevice()
 {
-    device = mdev_new("greedyMapper", port, 0);
-    if (!device) {
+    dev = mapper_device_new("greedyMapper", 0, 0);
+    if (!dev) {
         done = 1;
         return;
     }
-    while (!mdev_ready(device)) {
-        mdev_poll(device, 0);
-        usleep(50 * 1000);
+    while (!mapper_device_ready(dev)) {
+        mapper_device_poll(dev, 50);
     }
 }
 
-void startMonitor()
+void startDatabase()
 {
-    monitor = mapper_monitor_new();
-    if (!monitor) {
+    db = mapper_database_new(0, MAPPER_OBJ_MAPS);
+    if (!db) {
         done = 1;
         return;
     }
-    db = mapper_monitor_get_db(monitor);
-    mapper_db_add_link_callback(db, linkHandler, 0);
-    mapper_db_add_connection_callback(db, connectionHandler, 0);
+    mapper_database_add_map_callback(db, mapHandler, 0);
 }
 
 void cleanup()
 {
-    printf("\nReturning connections!\n");
-    mapper_db_connection_t **con = mapper_db_get_connections_by_src_dest_device_names(db,
-                                                mdev_name(device), mdev_name(device));
-    while (con) {
-        unsigned int flags = (((*con)->clip_min ? CONNECTION_CLIPMIN : 0) |
-                              ((*con)->clip_max ? CONNECTION_CLIPMAX : 0) |
-                              (*con)->range.known |
-                              ((*con)->expression ? CONNECTION_EXPRESSION : 0) |
-                              ((*con)->mode ? CONNECTION_MODE : 0) |
-                              ((*con)->muted ? CONNECTION_MUTED : 0));
-        mapper_monitor_connect(monitor, strchr((*con)->src_name+1, '/'), strchr((*con)->dest_name+1, '/'), *con, flags);
-        con = mapper_db_connection_next(con);
+    // need to copy properties from outgoing maps, but need to grab source signal from incoming maps
+    // solution: iterate through signals, for each, grab source, then iterate through signal's outgoing maps
+    printf("\nReturning maps!\n");
+    mapper_map map, *maps;
+    mapper_slot slot;
+    mapper_signal src, dst, *sigs = mapper_device_signals(dev, MAPPER_DIR_ANY);
+    while (sigs && *sigs) {
+        src = 0;
+        maps = mapper_signal_maps(*sigs, MAPPER_DIR_INCOMING);
+        if (maps) {
+            slot = mapper_map_slot(*maps, MAPPER_LOC_SOURCE, 0);
+            if (slot)
+                src = mapper_slot_signal(slot);
+        }
+        if (src) {
+            maps = mapper_signal_maps(*sigs, MAPPER_DIR_OUTGOING);
+            while (maps && *maps) {
+                dst = 0;
+                slot = mapper_map_slot(*maps, MAPPER_LOC_DESTINATION, 0);
+                if (slot)
+                    dst = mapper_slot_signal(slot);
+                if (dst) {
+                    map = mapper_map_new(1, &src, 1, &dst);
+                    int i, length, num_props = mapper_map_num_properties(*maps);
+                    char type;
+                    const char *name;
+                    const void *value;
+                    for (i = 0; i < num_props; i++) {
+                        mapper_map_property_index(*maps, i, &name, &length,
+                                                  &type, &value);
+                        mapper_map_set_property(map, name, length, type, value, 1);
+                    }
+                    mapper_map_push(map);
+                }
+                maps = mapper_map_query_next(maps);
+            }
+        }
+        sigs = mapper_signal_query_next(sigs);
     }
     printf("\nCleaning up!\n");
-    mdev_free(device);
-    mapper_monitor_free(monitor);
+    mapper_device_free(dev);
+    mapper_database_free(db);
 }
 
 void loop()
 {
     createDevice();
-    startMonitor();
-    mapper_monitor_link(monitor, mdev_name(device), mdev_name(device));
+    startDatabase();
     while (!done) {
         // poll libmapper outputs
-        mapper_monitor_poll(monitor, 0);
-        mdev_poll(device, 100);
+        mapper_database_poll(db, 0);
+        mapper_device_poll(dev, 100);
     }
 }
 
